68158a5b46e0b04b13636a447faa88f1f8fec4a9
roly <rmoya1223@gmail.com>
2026-04-08 09:52:36 -0500

    switch spanish and english

--- app/es/page.tsx
+++ app/es/page.tsx
@@ -3,7 +3,7 @@ import { PostcardFlip } from '../components/PostcardFlip';
 export default function SpanishPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-stone-100 dark:bg-stone-900 px-4">
-      <PostcardFlip backSrc="/2-2.jpeg" />
+      <PostcardFlip backSrc="/2-es.jpeg" />
     </div>
   );
 }
